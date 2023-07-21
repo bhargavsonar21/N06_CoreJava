ac32530fe5bfb84cea44057077838bbfc9d55718
User <User@BHARGAV-JARVIS-02>
2023-07-21 14:04:14 +0530

    Second Commit

---
+++ dayone/src/org/tns/operator/AssignmentOperatorDemo.java
@@ -0,0 +1,13 @@
+package org.tns.operator;
+//Program to Demonstrate Assignment Operator
+public class AssignmentOperatorDemo {
+
+	public static void main(String[] args) {
+		int x = 12;
+		int y = 7;
+		x-=y; //x = x-y
+		System.out.println(x);
+
+	}
+
+}
